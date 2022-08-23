85c45a6131af6511dc37bfe483ba07be2be5b384
iboks77 <iboks77@gmail.com>
2022-08-23 23:00:16 +0300

    add hw10

--- src/index.js
+++ src/index.js
@@ -2,18 +2,7 @@ import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import { Report } from 'notiflix/build/notiflix-report-aio';
-// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
-// import { Loading } from 'notiflix/build/notiflix-loading-aio';
-// import { Block } from 'notiflix/build/notiflix-block-aio';
-// Notiflix.Notify.success('Sol lucet omnibus');
 
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
-
-// Notiflix.Notify.warning('Memento te hominem esse');
-
-// Notiflix.Notify.info('Cogito ergo sum');
 const countryEl = document.querySelector('.country-list');
 const DEBOUNCE_DELAY = 300;
 
@@ -29,8 +18,7 @@ const paintMarkup = ({
   population,
   capital,
   languages,
-}) => {
-  return `<li class="country-info">
+}) => `<li class="country-info">
         <img class="flag" src="${svg}" alt="National Flag" width='240' height='160' >
         <h2 class="country-title">${common}</h2>
         <p class="country-text"><b>Capital:</b> ${capital}</p>
@@ -39,7 +27,6 @@ const paintMarkup = ({
           languages
         )}</p>
     </li>`;
-};
 const paintMarkup1 = ({ flags: { svg }, name: { common } }) => {
   return `<li class="country-info">
         <img class="flag" src="${svg}" alt="National Flag" width='240' height='160' >
@@ -48,18 +35,20 @@ const paintMarkup1 = ({ flags: { svg }, name: { common } }) => {
 };
 
 function onFindCountryInput(e) {
-  fetchCountries(e.target.value)
-    .then(data => data)
-    .then(data => {
-      // console.log(data.length);
-      if (data.length < 10 && data.length > 1) {
-        return (countryEl.innerHTML = data.map(paintMarkup1).join(''));
-      } else if (data.length === 1) {
-        return (countryEl.innerHTML = data.map(paintMarkup).join(''));
-      } else if (data.length > 10) {
-        Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      }
-    });
+  fetchCountries(e.target.value).then(data => {
+    if (data.length <= 10 && data.length > 1) {
+      return (countryEl.innerHTML = data.map(paintMarkup1).join(''));
+    }
+    if (data.length === 1) {
+      return (countryEl.innerHTML = data.map(paintMarkup).join(''));
+    }
+    if (data.length === undefined) {
+      return (countryEl.innerHTML = '');
+    }
+    if (e.target.value.trim() !== '') {
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+      );
+    }
+  });
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,25 +1,17 @@
 const URL = 'https://restcountries.com/v3.1/name';
 import Notiflix from 'notiflix';
 
-/*
-// const refs = {
-//   name: name,
-//   capital: capital,
-//   population: population,
-//   flags: flags,
-//   languages: languages,
-// };
-const URL = 'https://restcountries.com/v3'
-*/
 const FILTER_RESPONSE = 'name,capital,population,flags,languages';
 
 export function fetchCountries(name) {
   return fetch(`${URL}/${name}?fields=${FILTER_RESPONSE}`)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(() => {
-      if (e.target.value.trim() !== '') {
-        Notiflix.Notify.warning('Memento te hominem esse');
+    .then(response => {
+      if (response.status === 404) {
+        Notiflix.Notify.warning('Oops, there is no country with that name');
       }
+      return response.json();
+    })
+    .catch(error => {
+      console.error(error);
     });
 }
