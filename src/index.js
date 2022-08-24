1ebda193f6048c714fa6a19c27c34072fbdc0e4f
iboks77 <iboks77@gmail.com>
2022-08-24 19:06:52 +0300

    add img

--- src/index.js
+++ src/index.js
@@ -1,55 +1,104 @@
 import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries';
+import { getImage } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
-const countryEl = document.querySelector('.country-list');
-const DEBOUNCE_DELAY = 300;
+// const gallery = document.querySelector('.gallery');
 
-const inputFields = document.querySelector('#search-box');
-inputFields.addEventListener(
-  'input',
-  debounce(onFindCountryInput, DEBOUNCE_DELAY)
-);
+const inputForm = document.querySelector('#search-form');
+const galleryBox = document.querySelector('.gallery');
+inputForm.addEventListener('submit', onClickSubmitBtn);
+// const submitBtn = document.querySelector('[type="submit"]');
+// inputForm.addEventListener('input', onInputInForm);
+// const DEBOUNCE_DELAY = 300;
 
-const paintMarkup = ({
-  flags: { svg },
-  name: { common },
-  population,
-  capital,
-  languages,
-}) => `<li class="country-info">
-        <img class="flag" src="${svg}" alt="National Flag" width='210' height='140' >
-        <h2 class="country-title">${common}</h2>
-        <p class="country-text"><b>Capital:</b> ${capital}</p>
-        <p class="country-text"><b>Population:</b> ${population}</p>
-        <p class="country-text"><b>Languages:</b>  ${Object.values(
-          languages
-        )}</p>
-    </li>`;
-const paintMarkup1 = ({ flags: { svg }, name: { common } }) => {
-  return `<li class="country-info">
-        <img class="flag" src="${svg}" alt="National Flag" width='210' height='140' >
-        <h2 class="country-title">${common}</h2>
-         </li>`;
-};
-
-function onFindCountryInput(e) {
-  countryEl.innerHTML = '';
-  if (e.target.value.trim() !== '') {
-    fetchCountries(e.target.value).then(data => {
-      if (data.length <= 10 && data.length > 1) {
-        return (countryEl.innerHTML = data.map(paintMarkup1).join(''));
-      }
-      if (data.length === 1) {
-        return (countryEl.innerHTML = data.map(paintMarkup).join(''));
-      }
-      if (data.length === '') {
-        return (countryEl.innerHTML = '');
-      }
-      Notiflix.Notify.info(
-        'Too many matches found. Please enter a more specific name.'
-      );
-    });
-  }
+// getImage();
+async function onClickSubmitBtn(e) {
+  e.preventDefault();
+  const inputValue = inputForm.elements.searchQuery.value;
+  // console.log(inputForm.elements.searchQuery.value);
+  const response = await getImage(inputValue);
+  console.log(response.hits);
+  markup(response.hits);
+  return response;
 }
+
+function markup(result) {
+  result.map(item => {
+    const {
+      // id,
+      largeImageURL,
+      webformatURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    } = item;
+    galleryBox.innerHTML += `<a class="photo-link" href="${largeImageURL}"><div class="photo-card">
+  <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div></a>`;
+  });
+}
+
+// const inputFields = document.querySelector('#search-box');
+// inputFields.addEventListener(
+//   'input',
+//   debounce(onFindCountryInput, DEBOUNCE_DELAY)
+// );
+
+// const paintMarkup = ({
+//   flags: { svg },
+//   name: { common },
+//   population,
+//   capital,
+//   languages,
+// }) => `<li class="country-info">
+//         <img class="flag" src="${svg}" alt="National Flag" width='210' height='140' >
+//         <h2 class="country-title">${common}</h2>
+//         <p class="country-text"><b>Capital:</b> ${capital}</p>
+//         <p class="country-text"><b>Population:</b> ${population}</p>
+//         <p class="country-text"><b>Languages:</b>  ${Object.values(
+//           languages
+//         )}</p>
+//     </li>`;
+// const paintMarkup1 = ({ flags: { svg }, name: { common } }) => {
+//   return `<li class="country-info">
+//         <img class="flag" src="${svg}" alt="National Flag" width='210' height='140' >
+//         <h2 class="country-title">${common}</h2>
+//          </li>`;
+// };
+
+// function onFindCountryInput(e) {
+//   countryEl.innerHTML = '';
+//   if (e.target.value.trim() !== '') {
+//     fetchCountries(e.target.value).then(data => {
+//       if (data.length <= 10 && data.length > 1) {
+//         return (countryEl.innerHTML = data.map(paintMarkup1).join(''));
+//       }
+//       if (data.length === 1) {
+//         return (countryEl.innerHTML = data.map(paintMarkup).join(''));
+//       }
+//       if (data.length === '') {
+//         return (countryEl.innerHTML = '');
+//       }
+//       Notiflix.Notify.info(
+//         'Too many matches found. Please enter a more specific name.'
+//       );
+//     });
+//   }
+// }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,17 +1,39 @@
-const URL = 'https://restcountries.com/v3.1/name';
+import axios from 'axios';
 import Notiflix from 'notiflix';
 
-const FILTER_RESPONSE = 'name,capital,population,flags,languages';
+// const FILTER_RESPONSE = 'q,image_type=photo,orientation,safesearch=true';
+// const baseURL = 'https://pixabay.com/api/';
+// const KEY = '29479728-a98d2355de22f92bb93dea3e0';
 
-export function fetchCountries(name) {
-  return fetch(`${URL}/${name}?fields=${FILTER_RESPONSE}`)
-    .then(response => {
-      if (response.status === 404) {
-        Notiflix.Notify.warning('Oops, there is no country with that name');
-      }
-      return response.json();
-    })
-    .catch(error => {
-      console.error(error);
-    });
+/*
+`?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+*/
+
+// export async function fetchCountries() {
+//   try {
+//     const response = await axios.get(
+//       `${baseURL}?key=${KEY}&${FILTER_RESPONSE}`
+//     );
+//     return response;
+//   } catch (error) {
+//     console.log(error);
+//     // if (response.status === 404) {
+//     //   Notiflix.Notify.warning('Oops, there is no country with that name');
+//     // }
+//   }
+// }
+export async function getImage(inputValue) {
+  let options = {
+    params: {
+      key: '29479728-a98d2355de22f92bb93dea3e0',
+      q: inputValue,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: 1,
+      per_page: 40,
+    },
+  };
+  const response = await axios.get('https://pixabay.com/api/', options);
+  return response.data;
 }
